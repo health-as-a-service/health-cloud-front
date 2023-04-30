8a1e6dedc54d8c975b5431ad886d98e3f588f74a
Aymen Dhahri <mohamediheb.berraies@esprit.tn>
2023-04-30 03:21:59 +0100

    added edit operation

--- src/app/admin/operations/edit-operation/edit-operation.component.ts
+++ src/app/admin/operations/edit-operation/edit-operation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { ActivatedRoute } from "@angular/router";
 import { Subscription } from "rxjs";
 import { Operation } from "src/app/core/models/operation";
@@ -11,9 +12,13 @@ import { OperationService } from "src/app/core/service/operation.service";
 })
 export class EditOperationComponent implements OnInit {
   private id: number = 0;
-  private toUpdate: Operation;
   private sub: Subscription;
+  toUpdate: Operation;
+  editOpForm: FormGroup;
+  success: boolean = false;
+
   constructor(
+    private fb: FormBuilder,
     private route: ActivatedRoute,
     private opServ: OperationService
   ) {}
@@ -21,8 +26,7 @@ export class EditOperationComponent implements OnInit {
   ngOnInit() {
     this.sub = this.route.params.subscribe((params) => {
       this.id = +params["id"]; // (+) converts string 'id' to a number
-      console.log(this.id);
-    } );
+    });
     this.fetchData();
   }
 
@@ -30,11 +34,23 @@ export class EditOperationComponent implements OnInit {
     this.sub.unsubscribe();
   }
 
+  onSubmit() {
+    // fill toUpdate
+    // send it to backend
+    // display beautiful toast / sweetalert lol
+  }
+
   fetchData() {
     this.opServ.getOperationById(this.id).subscribe({
       next: (l) => {
-        this.toUpdate = l;
-        alert(JSON.stringify(l));
+        this.success = l.success;
+        this.editOpForm = this.fb.group({
+          nomChi: [l.nomChi, [Validators.required]],
+          nomP: [l.nomP, [Validators.required]],
+          dateOp: [l.dateOp, [Validators.required]],
+          typeOp: [l.typeOp, [Validators.required]],
+          emailP: [l.emailP, [Validators.required, Validators.email]],
+        });
       },
       error: (err) => {},
     });
--- src/app/admin/operations/operations.module.ts
+++ src/app/admin/operations/operations.module.ts
@@ -4,6 +4,26 @@ import { ListOperationsComponent } from "./list-operations/list-operations.compo
 import { AddOperationComponent } from "./add-operation/add-operation.component";
 import { EditOperationComponent } from "./edit-operation/edit-operation.component";
 import { OperationsRoutingModule } from "./operations-routing.module";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatSortModule } from "@angular/material/sort";
+import { MatTableModule } from "@angular/material/table";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MaterialFileInputModule } from "ngx-material-file-input";
+import { ComponentsModule } from "src/app/shared/components/components.module";
+import { SharedModule } from "src/app/shared/shared.module";
 
 @NgModule({
   declarations: [
@@ -11,6 +31,30 @@ import { OperationsRoutingModule } from "./operations-routing.module";
     AddOperationComponent,
     EditOperationComponent,
   ],
-  imports: [OperationsRoutingModule, CommonModule],
+  imports: [
+    OperationsRoutingModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSnackBarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule,
+    MatSortModule,
+    MatToolbarModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MaterialFileInputModule,
+    MatTabsModule,
+    MatProgressSpinnerModule,
+    ComponentsModule,
+    SharedModule,
+  ],
 })
 export class OperationsModule {}

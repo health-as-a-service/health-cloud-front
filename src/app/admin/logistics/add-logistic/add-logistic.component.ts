636d19d63b34a5a450b417d3c9c570f09b31158e
Mouhamed Iheb Berraies <mohamediheb.berraies@esprit.tn>
2023-05-06 02:09:17 +0100

    finished logistics

--- src/app/admin/logistics/add-logistic/add-logistic.component.ts
+++ src/app/admin/logistics/add-logistic/add-logistic.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ActivatedRoute } from "@angular/router";
+import { Subscription } from "rxjs";
+import { Logistique } from "src/app/core/models/logistique";
+import { LogisticsService } from "src/app/core/service/logistics.service";
 
 @Component({
-  selector: 'app-add-logistic',
-  templateUrl: './add-logistic.component.html',
-  styleUrls: ['./add-logistic.component.sass']
+  selector: "app-add-logistic",
+  templateUrl: "./add-logistic.component.html",
+  styleUrls: ["./add-logistic.component.sass"],
 })
 export class AddLogisticComponent implements OnInit {
+  private sub: Subscription;
+  editLogiForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private logServ: LogisticsService
+  ) {}
 
-  ngOnInit(): void {
+  onSubmit() {
+    let value = this.editLogiForm.value;
+
+    this.logServ.addLogistique(value).subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (err) => {
+        console.log("error updating operation!");
+        alert("Failed !");
+      },
+    });
   }
 
+  ngOnInit() {}
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 }
--- src/app/admin/logistics/edit-logistic/edit-logistic.component.ts
+++ src/app/admin/logistics/edit-logistic/edit-logistic.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { ActivatedRoute } from "@angular/router";
 import { Subscription } from "rxjs";
 import { Logistique } from "src/app/core/models/logistique";
@@ -12,16 +13,34 @@ import { LogisticsService } from "src/app/core/service/logistics.service";
 export class EditLogisticComponent implements OnInit {
   private id: number;
   private sub: Subscription;
-  private toUpdate: Logistique;
+  private fetched: Logistique;
+  editLogiForm: FormGroup;
+
   constructor(
+    private fb: FormBuilder,
     private route: ActivatedRoute,
     private logServ: LogisticsService
   ) {}
 
+  onSubmit() {
+    let value = this.editLogiForm.value;
+
+    value = { ...this.fetched, ...value };
+
+    this.logServ.updateLogistique(value).subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (err) => {
+        console.log("error updating operation!");
+        alert("Failed !");
+      },
+    });
+  }
+
   ngOnInit() {
     this.sub = this.route.params.subscribe((params) => {
       this.id = +params["id"]; // (+) converts string 'id' to a number
-      console.log(this.id);
     });
     this.fetchData();
   }
@@ -33,7 +52,12 @@ export class EditLogisticComponent implements OnInit {
   fetchData() {
     this.logServ.getLogistiqueById(this.id).subscribe({
       next: (l) => {
-        this.toUpdate = l;
+        this.fetched = l;
+        this.editLogiForm = this.fb.group({
+          typeLogi: [l.typeLogi, [Validators.required]],
+          nomLogi: [l.nomLogi, [Validators.required]],
+          nombreLogits: [l.nombreLogits, [Validators.required]],
+        });
       },
       error: (err) => {},
     });
